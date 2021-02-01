18a1466803a76996170b9da6c565ccf8531ccc43
Osskr <osskrjuarez@gmail.com>
2021-02-01 15:55:19 -0300

    terminado el navbar responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log('hola perri')
+const toggleMenuElement = document.getElementById('toggle-button')
+const headerElement = document.querySelector('.header')
+const overlayElement = document.querySelector('.overlay')
+
+toggleMenuElement.addEventListener('click', ()=>{
+
+    if(headerElement.classList.contains('open')){
+        //close hamburguer menu
+     
+        headerElement.classList.remove('open')
+        overlayElement.classList.add('fade-out')
+        overlayElement.classList.remove('fade-in')
+       
+    }else{
+        headerElement.classList.add('open') //open hamburguer menu
+        overlayElement.classList.add('fade-in')
+        overlayElement.classList.remove('fade-out')
+    }
+    
+      
+})
