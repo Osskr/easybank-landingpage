a3800011f6692ca710e1d0f8bc87d902e70623c0
Osskr <osskrjuarez@gmail.com>
2021-02-02 17:09:45 -0300

    terminada seccion navbar y menu con animaciones

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,31 @@
 const toggleMenuElement = document.getElementById('toggle-button')
 const headerElement = document.querySelector('.header')
 const overlayElement = document.querySelector('.overlay')
-
+const fadeElements  = document.querySelectorAll('.has-fade')
+const bodyElement = document.querySelector('body')
 toggleMenuElement.addEventListener('click', ()=>{
 
     if(headerElement.classList.contains('open')){
         //close hamburguer menu
-     
-        headerElement.classList.remove('open')
-        overlayElement.classList.add('fade-out')
-        overlayElement.classList.remove('fade-in')
+        bodyElement.classList.remove('noscroll')
+       headerElement.classList.remove('open')
+        fadeElements.forEach(el=>{
+            el.classList.add('fade-out')
+            el.classList.remove('fade-in')
+        })
+        
        
     }else{
+
         headerElement.classList.add('open') //open hamburguer menu
-        overlayElement.classList.add('fade-in')
-        overlayElement.classList.remove('fade-out')
+        bodyElement.classList.add('noscroll')
+        fadeElements.forEach((el)=>{
+            el.classList.add()
+            el.classList.add('fade-in')
+            el.classList.remove('fade-out')
+           
+        })
+       
     }
     
       
